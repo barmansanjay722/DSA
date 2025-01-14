d2bd4e8d06a6be82cb77ab8e4d1f7d7f9dad3f7e
sanjay <sanjay.barman@minditsystems.com>
2025-01-15 00:09:42 +0530

    backtracking permute answer fix

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-    }
 
-    static int matrixPath(int n, int m) {
-        if(n == 1 || m == 1) return 1;
-        return matrixPath(n-1, m) + matrixPath(n, m-1);
     }
 }
--- src/main/java/org/example/backtracking/PermuteOfArrayReturnArray.java
+++ src/main/java/org/example/backtracking/PermuteOfArrayReturnArray.java
@@ -2,21 +2,31 @@ package org.example.backtracking;
 
 import java.util.ArrayList;
 
-public class PermuteOfArray {
+public class PermuteOfArrayReturnArray {
     public static void main(String[] args) {                        //O(n! * n)
+        ArrayList<int[]> ans = new ArrayList<>();
 
         int a[] = {1,2,3};
-        permute(a,0,a.length-1);
+        permute(a,0,a.length-1,ans);
     }
-    static void permute(int a[], int l, int r) {
+    static void permute(int a[], int l, int r, ArrayList<int[]> ans) {
         if(l == r) {
             printArray(a);
+
+            // duplicate copy array of a[] array,
+            int[] b = new int[a.length];
+            for(int i=0;i<a.length;i++) {
+                b[i] = a[i];
+            }
+
+            // add array in ArrayList
+            ans.add(b);
             return;
         }
         for(int i=l;i<=r;i++) {
             swap(a,i,l);
-            permute(a,l+1,r);
-            swap(a,i,l);                // backtracking
+            permute(a,l+1,r,ans);
+            swap(a,i,l);                             // backtracking
         }
     }
 
